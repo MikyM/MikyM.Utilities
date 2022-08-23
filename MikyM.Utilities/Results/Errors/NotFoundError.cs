e7b21db2a5bccfc8d8c938f04caa24b2821732ea
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-08-23 10:59:25 +0200

    fix: remove results

--- MikyM.Utilities/Results/Errors/NotFoundError.cs
+++
@@ -1,8 +0,0 @@
-namespace MikyM.Utilities.Results.Errors;
-
-/// <summary>
-/// Represents a failure to find something that was searched for.
-/// </summary>
-/// <param name="Message">The custom message to provide.</param>
-[PublicAPI]
-public record NotFoundError(string Message = "The searched-for entity was not found.") : ResultError(Message);
--- MikyM.Utilities/Results/ResultError.cs
+++
@@ -1,7 +0,0 @@
-namespace MikyM.Utilities.Results;
-
-/// <summary>
-/// Acts as a base class for result errors.
-/// </summary>
-[PublicAPI]
-public abstract record ResultError(string Message) : IResultError;
